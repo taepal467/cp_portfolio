3629a67fe664ac88abea91e6fa6b2fb78095925e
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-22 15:19:23 -0800

    Added Google font and code for the dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        gloock: "Gloock-Regular",
+        greatVibes: 'Great Vibes',
+      }
+    },
   },
   plugins: [],
 }
