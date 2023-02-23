e61c33c6968c02ed3c003f52bb68ebac00919d22
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-22 17:02:50 -0800

    Removed the Great Vibes Google Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,17 @@ module.exports = {
     extend: {
       fontFamily: {
         gloock: "Gloock-Regular",
-        greatVibes: 'Great Vibes',
+      },
+      animation:{
+        loader: 'loader 0.6s infinite alternate'
+      },
+      keyframes: {
+        loader: {
+          to: {
+            opacity: 0.1,
+            transform: 'translate3d(0, -1rem, 0)'
+          }
+        }
       }
     },
   },
