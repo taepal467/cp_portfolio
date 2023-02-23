d123231e1d3e9fb5ab0460c846614d57cb7dab48
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-22 17:17:42 -0800

    Cleaned up the code I no longer need

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,17 +10,6 @@ module.exports = {
     extend: {
       fontFamily: {
         gloock: "Gloock-Regular",
-      },
-      animation:{
-        loader: 'loader 0.6s infinite alternate'
-      },
-      keyframes: {
-        loader: {
-          to: {
-            opacity: 0.1,
-            transform: 'translate3d(0, -1rem, 0)'
-          }
-        }
       }
     },
   },
