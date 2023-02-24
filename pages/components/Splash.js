a954f2f4bb4185f14f94e1e38af1ff6ac69eb51a
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-24 13:30:35 -0800

    Changed the animation for the Splash screen

--- pages/components/Splash.js
+++ pages/components/Splash.js
@@ -2,10 +2,14 @@ import Head from 'next/head'
 
 export default function Splash() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-900">
+    <div className="flex items-center justify-center h-screen bg-black">
       <Head>
-        <title>Loading...</title>
+        <title>CP</title>
       </Head>
+      <div className='flex justify-center'>
+        <span className=' text-cyan-100 text-7xl font-gloock animate-loader'>C</span>
+        <span className=' text-cyan-100 text-7xl font-gloock animate-loader animation-delay-200'>P</span>
+      </div>
     </div>
   )
 }
