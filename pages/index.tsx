ce636b601e9434b3a282f84ebb78b06cd3c3f9d0
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-26 12:15:45 -0800

    Removed the Projects component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import {BsFillMoonStarsFill, BsFillBrightnessHighFill, BsLinkedin, BsGithub} from 'react-icons/bs' ;
 import {FaDev} from 'react-icons/fa';
 import Link from 'next/link';
-import Projects from "./components/Projects";
 import { useState , useEffect} from 'react';
 import Splash from "./components/Splash";
 
@@ -30,7 +29,7 @@ export default function Home() {
         <Splash />
       ) : (
         <main className='px-10 bg-white font-gloock dark:bg-black'>
-          <section className='min-h-screen '>
+          <section className='min-h-screen border-2 border-b-black border-r-0 border-t-0 border-l-0'>
             <nav className='py-4 mb-12 flex justify-between'>
               <Link href="/">
                   <h1 className='text-3xl font-bold p-1 border-4 border-violet-300 border-r-black border-4 border-violet-300 border-b-black hover:shadow-lg dark:hover:shadow-black/30'>CP</h1>
@@ -51,7 +50,7 @@ export default function Home() {
                 <li className=' px-2 text-2xl font-bold transition ease-in-out delay-150 hover:underline underline-offset-4 decoration-4 decoration-violet-300 hover:-translate-y-1 hover:scale-110 hover:duration-300'>
                   <Link href='./components/Projects'>Projects</Link>
                   </li>
-                <li className=' px-2 text-2xl font-bold rounded-lg border-2 border-black bg-gradient-to-r from-violet-300 to-violet-400 hover:shadow-lg dark:hover:shadow-black/30'>
+                <li className=' px-2 text-2xl font-bold rounded-lg shadow-lg shadow-indigo-500/40 border-2 border-black bg-gradient-to-r from-violet-300 to-violet-400 hover:shadow-lg dark:hover:shadow-black/30'>
                   <a href="https://docs.google.com/document/d/1gd_s5Lm468wk0DONMSpgxVsJarLlb9Cx51AHrZmtRnQ/edit?usp=sharing" 
                   target='_blank'
                   rel="noopener noreferrer">
@@ -79,7 +78,8 @@ export default function Home() {
           </section>
         </main>
       )}
-   
+   <div>
+   </div>
     </div>
 
   );
