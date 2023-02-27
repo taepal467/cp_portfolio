8d45951c2e16b3c385f563a18db947f63c1a1ff4
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-26 18:27:44 -0800

    imported the AnimatePresence from framer motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AnimatePresence } from 'framer-motion'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }: AppProps) {
+  return (
+    <>
+      <AnimatePresence>
+        <Component key={router.pathname} {...pageProps} />
+      </AnimatePresence>
+    </>
+  );
+
 }
