c4ae7675c88f14177a4fb615dcfc0777e59e96df
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-28 01:03:31 -0800

    Added the useEffect hook to the _app.tsx. This makes the splash screen run only once and not when the home button is clicked all the time.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,25 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AnimatePresence } from 'framer-motion'
+import { useState, useEffect } from 'react'
+import Splash from "./components/Splash";
 
 export default function App({ Component, pageProps, router }: AppProps) {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+      const timer = setTimeout(() => setLoading(false), 3000);
+      return () => clearTimeout(timer);
+    }, []) 
   return (
     <>
+
+{loading ? <Splash /> : (
       <AnimatePresence>
         <Component key={router.pathname} {...pageProps} />
       </AnimatePresence>
+)}
     </>
   );
 
