5ed362e4c49d81dcdd34a29299a0752efd93bcef
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-27 18:23:57 -0800

    Tried making the splash screen load only once. Still dont get it

--- pages/components/Splash.js
+++ pages/components/Splash.js
@@ -1,14 +1,16 @@
 import Head from 'next/head'
 import Footer from "./Footer";
+import { useEffect, useState } from 'react';
 
 export default function Splash() {
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-black">
       <Head>
         <title>CP</title>
       </Head>
       <div className='flex justify-center pt-48 pb-72'>
-        <span className=' text-cyan-100 text-7xl font-gloock animate-loader'>C</span>
+        <span className=' text-cyan-100 text-7xl font-gloock animate-loader' >C</span>
         <span className=' text-cyan-100 text-7xl font-gloock animate-loader animation-delay-200'>P</span>
       </div>
         <Footer />
