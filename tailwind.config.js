9270af2791794fffce3efd281e3b36166e327184
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-28 17:01:46 -0800

    Changed the fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,20 +9,11 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        gloock: "Gloock-Regular",
+        sans: [' "Alumni Sans Pinstripe" ', 'sans-serif'],
       },
-      animation: {
-        loader: 'loader 0.6s infinite alternate'
-      },
-      keyframes: {
-        loader: {
-          to: {
-            opacity: 0.1,
-            transform: 'translate3d(0, -1rem, 0)'
-          }
-        }
-      },
+      
     },
   },
   plugins: [],
 }
+
