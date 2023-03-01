6607a8b4251e88ef71dcdd9dd95e919d2374340d
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-28 17:00:18 -0800

    Changed font and font color

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     let year =  new Date().getFullYear();
     return(
         <div className="flex pt-36">
-            <p className="text-black text-sm font-light">Created and developed By Chantae Palacio &copy; {year}</p>
+            <p className="text-gray-600 text-sm font-light">Created and developed By Chantae Palacio &copy; {year}</p>
         </div>
     );
 }
