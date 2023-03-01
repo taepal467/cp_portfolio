8656ca35c8013d808867f8dc14835015d4b01c23
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-28 16:30:59 -0800

    Finished styling and positioning the Footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,8 +1,8 @@
 export default function Footer() {
     let year =  new Date().getFullYear();
     return(
-        <div>
-            <p className="text-white">Created and developed By Chantae Palacio &copy; {year}</p>
+        <div className="flex pt-36">
+            <p className="text-black text-sm font-light">Created and developed By Chantae Palacio &copy; {year}</p>
         </div>
     );
 }
