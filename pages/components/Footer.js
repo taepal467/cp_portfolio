52918441749591a3adc225122e11cefccc95af29
Chantae Palacio <chantae.palacio@gmail.com>
2023-02-28 17:54:28 -0800

    Had to change the font size of the footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     let year =  new Date().getFullYear();
     return(
         <div className="flex pt-36">
-            <p className="text-gray-600 text-sm font-light">Created and developed By Chantae Palacio &copy; {year}</p>
+            <p className="text-gray-600 text-xl">Created and developed By Chantae Palacio &copy; {year}</p>
         </div>
     );
 }
