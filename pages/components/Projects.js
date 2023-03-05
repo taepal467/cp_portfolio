b387b923b882c4c0a89a68062ba4649079adf239
Chantae Palacio <chantae.palacio@gmail.com>
2023-03-04 16:16:26 -0800

    Figured out how to show the selected compoent on the Project page by using dynamic imports

--- pages/components/Projects.js
+++ pages/components/Projects.js
@@ -1,15 +1,30 @@
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
+import Link from 'next/link'
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+
+const DynamicComponent1 = dynamic(() => import('./cards/libraryCard'));
+const DynamicComponent2 = dynamic(() => import('./cards/weatherCard'));
+const DynamicComponent3 = dynamic(() => import('./cards/boutiqueCard'));
+const DynamicComponent4 = dynamic(() => import('./cards/cvCard'));
+const DynamicComponent5 = dynamic(() => import('./cards/luxuryCard'));
 
 export default function Projects() {
+
+    const [selectedComponent, setSelectedComponent] = useState(null);
+
+    function handleLinkClick(componentName) {
+        setSelectedComponent(componentName)
+    }
+
 
     return(
         <div >
            <motion.main 
-          className='bg-slate-100  text-gray-700 absolute top-0 left-0 w-full h-full p-5 font-sans dark:bg-slate-900 dark:text-slate-500'
+          className='bg-slate-100 text-gray-700 absolute top-0 left-0 w-full h-full p-5 font-sans dark:bg-slate-900 dark:text-slate-500'
           initial={{ opacity:0 }}
           animate={{ opacity: 1}}
           transition={{ duration: 0.75, ease: "easeOut"}}
-          exit={{ opacity: 1 }}
         >
             <motion.h1 
                 className=' text-7xl  pb-10 font-monoton'
@@ -17,45 +32,31 @@ export default function Projects() {
                 transition={{ ease: "easeOut", duration: 2 }}>
                 Projects
               </motion.h1>
-                    <section> 
-                        <div className="text-center shadow-lg p-10 rounded-xl my-10">
-                            <h3 className="text-lg font-medium pt-8 pb-2">CV Builder</h3>
-                            <p className="py-2">** project description **</p>
-                            <h4 className="py-4 text-gray-500">Technologies I used:</h4>
-                            <p className="py-1 text-gray-700">React</p>
-                        </div>
+                    <div className='mt-20'> 
+                    <ul>
+        <li>
+          <Link href="#" onClick={() => handleLinkClick('libraryCard')}>Library</Link>
+        </li>
+        <li>
+          <Link href="#" onClick={() => handleLinkClick('weatherCard')}>Weather App</Link>
+        </li>
+        <li>
+          <Link href="#" onClick={() => handleLinkClick('boutiqueCard')}>Tae Ice cream boutique</Link>
+        </li>
+        <li>
+          <Link href="#" onClick={() => handleLinkClick('cvCard')}>CV Builder</Link>
+        </li>
+        <li>
+          <Link href="#" onClick={() => handleLinkClick('luxuryCard')}>Tae luxury discount store</Link>
+        </li>
+      </ul>
 
-                        <div className="text-center shadow-lg p-10 rounded-xl my-10">
-                            <h3 className="text-lg font-medium pt-8 pb-2">Taes Ice Cream Butiquw</h3>
-                            <p className="py-2">** project description **</p>
-                            <h4 className="py-4 text-gray-500">Technologies I used:</h4>
-                            <p className="py-1 text-gray-700">Webpack</p>
-                            <p className="py-1 text-gray-700">CSS</p>
-                        </div>
-
-                        <div className="text-center shadow-lg p-10 rounded-xl my-10">
-                            <h3 className="text-lg font-medium pt-8 pb-2">Taes Luxury Discount Store</h3>
-                            <p className="py-2">** project description **</p>
-                            <h4 className="py-4 text-gray-500">Technologies I used:</h4>
-                            <p className="py-1 text-gray-700">React</p>
-                        </div>
-
-                        <div className="text-center shadow-lg p-10 rounded-xl my-10">
-                            <h3 className="text-lg font-medium pt-8 pb-2">CV Builder</h3>
-                            <p className="py-2">** project description **</p>
-                            <h4 className="py-4 text-gray-500">Technologies I used:</h4>
-                            <p className="py-1 text-gray-700">React</p>
-                        </div>
-
-                        <div className="text-center shadow-lg p-10 rounded-xl my-10">
-                            <h3 className="text-lg font-medium pt-8 pb-2">The Weather App</h3>
-                            <p className="py-2">** project description **</p>
-                            <h4 className="py-4 text-gray-500">Technologies I used:</h4>
-                            <p className="py-1 text-gray-700">openWeather API</p>
-                            <p className="py-1 text-gray-700">JavaScript</p>
-                        </div>
-                     
-                    </section>
+      {selectedComponent === 'libraryCard' && <DynamicComponent1 />}
+      {selectedComponent === 'weatherCard' && <DynamicComponent2 />}
+      {selectedComponent === 'boutiqueCard' && <DynamicComponent3 />}
+      {selectedComponent === 'cvCard' && <DynamicComponent4 />}
+      {selectedComponent === 'luxuryCard' && <DynamicComponent5 />}
+                    </div>
                     
             </motion.main>
         </div>
