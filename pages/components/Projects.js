27fb882e0c615863e7c7f98b125b12e47ec5338f
Chantae Palacio <chantae.palacio@gmail.com>
2023-03-04 17:35:21 -0800

    Work in progress

--- pages/components/Projects.js
+++ pages/components/Projects.js
@@ -21,42 +21,44 @@ export default function Projects() {
     return(
         <div >
            <motion.main 
-          className='bg-orange-100 text-gray-700 absolute top-0 left-0 w-full h-full p-5 font-sans dark:bg-neutral-900 dark:text-neutral-500'
-          initial={{ opacity:0 }}
-          animate={{ opacity: 1}}
-          transition={{ duration: 0.75, ease: "easeOut"}}
-        >
+            className='bg-orange-100 text-gray-700 absolute top-0 left-0 w-full h-full p-5 font-sans dark:bg-neutral-900 dark:text-neutral-500'
+            initial={{ opacity:0 }}
+            animate={{ opacity: 1}}
+            transition={{ duration: 0.75, ease: "easeOut"}}
+            >
             <motion.h1 
-                className=' text-7xl  pb-10 font-poiret'
-                animate={{ y: 30 }}
-                transition={{ ease: "easeOut", duration: 2 }}>
-                Projects
-              </motion.h1>
-                    <div className='mt-20'> 
-                    <ul>
-        <li>
-          <Link href="#" onClick={() => handleLinkClick('libraryCard')}>Library</Link>
-        </li>
-        <li>
-          <Link href="#" onClick={() => handleLinkClick('weatherCard')}>Weather App</Link>
-        </li>
-        <li>
-          <Link href="#" onClick={() => handleLinkClick('boutiqueCard')}>Tae Ice cream boutique</Link>
-        </li>
-        <li>
-          <Link href="#" onClick={() => handleLinkClick('cvCard')}>CV Builder</Link>
-        </li>
-        <li>
-          <Link href="#" onClick={() => handleLinkClick('luxuryCard')}>Tae luxury discount store</Link>
-        </li>
-      </ul>
-
-      {selectedComponent === 'libraryCard' && <DynamicComponent1 />}
-      {selectedComponent === 'weatherCard' && <DynamicComponent2 />}
-      {selectedComponent === 'boutiqueCard' && <DynamicComponent3 />}
-      {selectedComponent === 'cvCard' && <DynamicComponent4 />}
-      {selectedComponent === 'luxuryCard' && <DynamicComponent5 />}
-                    </div>
+            className=' text-7xl  pb-10 font-poiret'
+            animate={{ y: 30 }}
+            transition={{ ease: "easeOut", duration: 2 }}>
+            Projects
+            </motion.h1>
+            <div className='mt-20'> 
+                <ul>
+                    <li>
+                    <Link href="#" onClick={() => handleLinkClick('libraryCard')}>Library</Link>
+                    </li>
+                    <li>
+                    <Link href="#" onClick={() => handleLinkClick('weatherCard')}>Weather App</Link>
+                    </li>
+                    <li>
+                    <Link href="#" onClick={() => handleLinkClick('boutiqueCard')}>Tae Ice cream boutique</Link>
+                    </li>
+                    <li>
+                    <Link href="#" onClick={() => handleLinkClick('cvCard')}>CV Builder</Link>
+                    </li>
+                    <li>
+                    <Link href="#" onClick={() => handleLinkClick('luxuryCard')}>Tae luxury discount store</Link>
+                    </li>
+                </ul>
+                <div className='lg:absolute lg:inset-x-120 lg:right-0'>
+                {selectedComponent === 'libraryCard' && <DynamicComponent1 />}
+                {selectedComponent === 'weatherCard' && <DynamicComponent2 />}
+                {selectedComponent === 'boutiqueCard' && <DynamicComponent3 />}
+                {selectedComponent === 'cvCard' && <DynamicComponent4 />}
+                {selectedComponent === 'luxuryCard' && <DynamicComponent5 />}
+                </div>
+                
+            </div>
                     
             </motion.main>
         </div>
