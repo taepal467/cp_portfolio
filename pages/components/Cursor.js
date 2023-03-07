d2ad5fdb8055d61e288d4a5bf8169c316e3b0e97
Chantae Palacio <chantae.palacio@gmail.com>
2023-03-07 10:42:47 -0800

    Tried creating a cursor hover effect. Deleted the code

--- pages/components/Cursor.js
+++ pages/components/Cursor.js
@@ -7,7 +7,7 @@ export default function Cursor() {
         y:0,
         x:0
       });
-   
+ 
       useEffect(() => {
         const mouseMove = (e) => {
           setMousePosition({
@@ -34,6 +34,7 @@ export default function Cursor() {
         <motion.div 
         className='cursor bg-black h-5 w-5 rounded-full fixed top-0 left-0 opacity-60 dark:bg-champagne'
         variants={variants}
-        animate='default'></motion.div>
+        animate='default'>
+        </motion.div>
     );
 }
